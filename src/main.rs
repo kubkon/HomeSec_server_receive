2367f7ab5c62168b419fea8c71f890f3dc388155
Jakub Konka <kubkon@jakubkonka.com>
2017-03-26 11:19:17 +0100

    Parse reed value (array of bytes in big endian into u16);

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,34 @@
+extern crate byteorder;
+extern crate chrono;
+
+use byteorder::{ReadBytesExt,BigEndian};
+use chrono::{DateTime,UTC};
+use std::io::Cursor;
 use std::net::UdpSocket;
-use std::time::SystemTime;
 
 fn main() {
     println!("Listening for packets...");
     loop {
         let socket = UdpSocket::bind("192.168.0.12:8888").expect("couldn't bind to address");
-        let mut buf = [0; 2];
+        let mut buf = [0; 4];
         socket.recv_from(&mut buf).expect("didn't receive any data");
-        let now = SystemTime::now();
+        let now: DateTime<UTC> = UTC::now();
 
         match buf[0] {
             0x1 => { // reed
-                match buf[1] {
-                    0x1 => println!("Door open at {:?}", now),
-                    0x2 => println!("Door close at {:?}", now),
+                let state = buf[1];
+                let mut raw_value = Cursor::new(vec![buf[2] & 0x3, buf[3]]);
+                let value = raw_value.read_u16::<BigEndian>().unwrap();
+                match state {
+                    0x1 => println!("Door open at {}; pin value={}", now, value),
+                    0x2 => println!("Door close at {}; pin value={}", now, value),
                     _ => println!("Unknown reed state received"),
                 }
             },
             0x2 => { // pir
                 match buf[1] {
-                    0x1 => println!("Motion detected at {:?}", now),
-                    0x2 => println!("Motion ended at {:?}", now),
+                    0x1 => println!("Motion detected at {}", now),
+                    0x2 => println!("Motion ended at {}", now),
                     _ => println!("Unknown PIR state received"),
                 }
             },
